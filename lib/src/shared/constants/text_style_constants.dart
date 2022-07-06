83b30a66a7114b82dac2a11dddf6c74b05fd2f2b
pham thang <thangpn@its-global.vn>
2022-07-06 23:41:18 +0700

    fix text

--- lib/src/shared/constants/text_style_constants.dart
+++ lib/src/shared/constants/text_style_constants.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 import 'colors_constants.dart';
 
 class AppTextStyle {
-  static final appBarTitle = customTextStyle(FontWeight.w400, 14, textAppbar);
-  static final heading1Bold = customTextStyle(FontWeight.w700, 18, textNormal);
-  static final heading2Bold = customTextStyle(FontWeight.w700, 16, textNormal);
-  static final heading3 = customTextStyle(FontWeight.w500, 14, textNormal);
-  static final heading3Bold = customTextStyle(FontWeight.bold, 14, textNormal);
-  static final body1 = customTextStyle(FontWeight.w400, 12, textNormal);
-  static final body1Medium = customTextStyle(FontWeight.w600, 12, textNormal);
-  static final body1Bold = customTextStyle(FontWeight.w700, 14, textNormal);
-  static final caption1 = customTextStyle(FontWeight.w400, 12, textNormal);
+  static final appBarTitle = customTextStyle(FontWeight.w400, 12, textAppbar);
+  static final heading1Bold = customTextStyle(FontWeight.w700, 16, textNormal);
+  static final heading2Bold = customTextStyle(FontWeight.w700, 14, textNormal);
+  static final heading3 = customTextStyle(FontWeight.w500, 12, textNormal);
+  static final heading3Bold = customTextStyle(FontWeight.bold, 12, textNormal);
+  static final body1 = customTextStyle(FontWeight.w400, 10, textNormal);
+  static final body1Medium = customTextStyle(FontWeight.w600, 10, textNormal);
+  static final body1Bold = customTextStyle(FontWeight.w700, 10, textNormal);
+  static final caption1 = customTextStyle(FontWeight.w400, 10, textNormal);
 }
 
 TextStyle customTextStyle(FontWeight fontWeight, double size, Color color,
